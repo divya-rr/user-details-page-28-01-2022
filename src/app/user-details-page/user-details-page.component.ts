14613ee7d33de54e925e870355521931b21ae478
divya-rr <divya.r@codestoresolutions.com>
2022-01-31 19:21:40 +0530

    password and forbiddenName validator

--- src/app/user-details-page/user-details-page.component.ts
+++ src/app/user-details-page/user-details-page.component.ts
@@ -1,34 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../services/user.service';
+import { ForbiddenNameValidator } from '../shared/forbiddenName.validator';
+import { passwordValidator } from '../shared/password.validator';
+
 
 @Component({
   selector: 'app-user-details-page',
   templateUrl: './user-details-page.component.html',
   styleUrls: ['./user-details-page.component.css']
 })
-export class UserDetailsPageComponent implements OnInit {
-
-userForm=new FormGroup({
-  name: new FormControl('',[Validators.required,Validators.pattern("^(?!)[a-zA-Z]*(?<!)$")]),
-  email:new FormControl('',Validators.email),
-  password: new FormControl('',[Validators.minLength(8),Validators.pattern("^[a-zA-Z]+$")]),
-  phone: new FormControl('',[Validators.pattern("[7-9]\d{9}"),Validators.minLength(10),Validators.maxLength(10)])
-})
-
-  // public userForm!: FormGroup;
-  // constructor(private fb:FormBuilder){
-  //   this.userForm=fb.group({
-  //     name:['',Validators.required],
-  //     email:['',Validators.required],
-  //     mobileNumber:['',Validators.required],
-  //   })
-
-  // }
+export class UserDetailsPageComponent{
+  constructor(private userService:UserService){}
+  userForm!: FormGroup;
+  
+  
 
   
 
   ngOnInit(): void {
+    this.userForm=new FormGroup({
+      name: new FormControl('',[Validators.required,Validators.pattern('^([a-zA-Z]+    )+[a-zA-Z]+$|^[A-Za-z]+$'),ForbiddenNameValidator(/test/)]),
+      email:new FormControl('',
+      {validators:[Validators.email,Validators.required,Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+$")],
+    asyncValidators:[this.userService.uniqueEmailValidator()]},),
+      password: new FormControl('',[Validators.required,Validators.pattern("^(?=.*[A-Z])(?=.*[a-z])(?=.*[@$!%*?&]).{8}$")]),
+      //Validators.pattern("^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$")
+      phone: new FormControl('',[Validators.required,Validators.pattern("[1-9][0-9]{9}")]),
+      confirmPassword:new FormControl('',{validators:passwordValidator})
+    })
   }
+ 
+  
   get name(){
     return this.userForm.get('name')
   }
@@ -40,4 +43,8 @@ userForm=new FormGroup({
   get password(){
     return this.userForm.get('password')
   }
+  get confirmPassword(){
+    return this.userForm.get('confirmPassword')
+  }
+
 }
