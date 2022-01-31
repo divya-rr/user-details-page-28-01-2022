4113a25a7ccd4bc029b236f5b613a36f54c6ef52
divya-rr <divya.r@codestoresolutions.com>
2022-01-31 19:22:42 +0530

    forbiddenname

---
+++ src/app/shared/forbiddenName.validator.ts
@@ -0,0 +1,13 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+
+
+export function ForbiddenNameValidator 
+  (nameRe: RegExp): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const forbidden = nameRe.test(control.value);
+      return forbidden ? {forbiddenName: {value: control.value}} : null;
+    };
+
+
+
+}
